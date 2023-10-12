a2dfefb5dd139a7f29111d8e4f0afbb80dc47e22
thienms98 <thienms98@gmail.com>
2023-10-12 17:52:30 +0700

    replace with yarn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
 import React from 'react';
 
 const DashBoard = () => {
+  const session = useSession();
+  console.log(session);
   return <div>DashBoard</div>;
 };
 
