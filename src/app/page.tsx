778145dc6ef799c92ab9f940ce9dfc33854caee5
thienms98 <thienms98@gmail.com>
2023-10-17 16:06:40 +0700

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  router.push('/dashboard');
+  redirect('/dashboard');
 }
