ff9b68d3c875b3a5043f89aebb7624639e3491b4
thienms98 <thienms98@gmail.com>
2023-10-20 16:28:53 +0700

    remove favicon

--- src/app/api/online-users/route.ts
+++ src/app/api/online-users/route.ts
@@ -2,12 +2,12 @@ import jwt from "@/lib/jwt";
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest){
+export async function GET(){
   const tokens = await prisma.account.findMany({
     select: {username:true, token: true}
   })
 
-  const valid = tokens.filter(({username, token}) => {
+  const valid = tokens.filter(({token}) => {
     if(!token) return false
     try{
       const data = jwt.verify(token)
