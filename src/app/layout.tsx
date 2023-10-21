1d16383508331831bd76d4bd59f792a7082003b1
thienms98 <thienms98@gmail.com>
2023-10-21 16:16:46 +0700

    add env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta property="og:image" content="/livingroom.jpeg" />
         <meta property="og:image:alt" content="Livingroom" />
 
